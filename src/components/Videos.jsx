bd15f6022250dfc31c73f2ff213a2d520bb27137
Ashit Rai <raiashit669@gmail.com>
2024-09-14 17:29:27 +0530

    #

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -7,7 +7,7 @@ const Videos = ({ videos, direction }) => {
   if(!videos?.length) return 'Loading...';
   
   return (
-    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={1}>
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} /> }
